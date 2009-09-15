a4773515c95dfcf3557dee7cca397205fa4afb72
Steel Head <steelhead@developer-ones-macbook.local>
2009-09-15 14:20:04 -0700

    more org-mode changes

--- my-org-mode.el
+++ my-org-mode.el
@@ -1,2 +1,9 @@
+(load "more-my-org-mode/org-mac-message")
+
+(when (file-exists-p "~/steelhead")
+  (let ((org-mac-mail-account "spawnordie"))
+    (global-set-key "\C-cm" 
+                    'org-mac-message-insert-selected)))
+
 (setq org-todo-keywords
-      '((sequence "TODO(t)" "WAIT(w@/!)" "FIXME(f)" "|" "DONE(d@!)" "CANCELED(c@)")))
+      '((sequence "TODO(t!)" "INPROGRESS(p!)" "WAIT(w@/!)" "FIXME(f)" "FUTURE(u@!)" "|" "DONE(d@/!)" "CANCELED(c@)")))
