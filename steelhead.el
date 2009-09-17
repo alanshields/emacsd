3fa34e85b47a9ac7bdfdea0f44f68ca007f77d73
Alan Shields <ashields@spawnordie.com>
2009-09-17 10:40:12 -0700

    more fixes

--- steelhead.el
+++ steelhead.el
@@ -1,16 +1,21 @@
 ;; php mode hook
 (defun steelhead-php-style ()
   (interactive)
+  ; Steelhead uses tabs, not spaces.
+  (setq indent-tabs-mode t)
   ; Steelhead uses javadoc comments like so:
   ; /*
   ; * something here
   ; */
   (add-to-list 'c-offsets-alist '(c . c-lineup-comment))
-  ; Steelhead uses tabs, not spaces.
-  (setq indent-tabs-mode t)
-  ; Steelhead uses 4 space indentation
-  (setq c-basic-offset 4)
-  (setq tab-width 4))
+  ; Allman style (AKA bsd) - but for some reason this isn't working. Let's keep moving.
+  (c-set-style "bsd" nil)
+
+  ; Electric parens seems to break with php-enhanced-mode
+  (setq c-electric-flag nil)
+  ; Mumamo on
+  (nxhtml-mumamo-mode))
+ 
 
 (add-hook 'php-mode-hook
           #'steelhead-php-style)
