6f2ba5cf140cef021fae3cb0606b5abea4630514
Alan Shields <alan@alan.shields.name>
2009-09-28 22:59:30 -0700

    Fix for byte compiling

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # for macs
-/Applications/Emacs.app/Contents/MacOS/Emacs -batch -f my-byte-compile-initd
+/Applications/Emacs.app/Contents/MacOS/Emacs -batch -nw -l ~/.emacs.d/init.el --eval '(my-byte-compile-initd)'
