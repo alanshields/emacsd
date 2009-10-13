05455fb6c460dce0e1eb651dfffcf18fa7e820d0
Alan Shields <alan@alan.shields.name>
2009-10-13 13:45:14 -0700

    my-org-mode: add commafy and uncommafy number

--- my-org-mode.el
+++ my-org-mode.el
@@ -1,5 +1,24 @@
 (load "more-my-org-mode/org-mac-message")
 
+(defun commafy-number (n)
+  "Put commas in an integer"
+  (let ((chunks ()))
+    (do ((chunk (mod (abs n) 1000) (mod rest 1000))
+         (rest (floor (abs n) 1000) (floor rest 1000)))
+        ((and (zerop chunk) (zerop rest)))
+      (push (number-to-string chunk) chunks)
+      (when (< chunk 100) (push "0" chunks))
+      (when (< chunk 10) (push "0" chunks))
+      (when (plusp rest) (push "," chunks)))
+    (while (string= (first chunks) "0") (pop chunks))
+    (when (minusp n) (push "-" chunks))
+    (apply 'concat chunks)))
+
+(defun uncommafy-number (str)
+  (if (numberp str)
+      str
+    (float (string-to-number (remove ?, str)))))
+
 (when (file-exists-p "~/steelhead")
   (let ((org-mac-mail-account "spawnordie"))
     (global-set-key "\C-cm" 
