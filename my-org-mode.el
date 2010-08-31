4805423a88e2af803da26d42a2f43cc6c0052649
Alan Shields <ashields@spawnordie.com>
2010-08-31 13:07:37 -0700

    Org mode updates

--- my-org-mode.el
+++ my-org-mode.el
@@ -27,4 +27,30 @@
 (setq org-todo-keywords
       '((sequence "TODO(t!)" "INPROGRESS(p!)" "WAIT(w@/!)" "FIXME(f)" "FUTURE(u@!)" "|" "DONE(d@/!)" "CANCELED(c@!)")))
 
+(defun send-rhi-email (email-text email-subject)
+  (let ((email-text (concat email-text "\nThanks,\nAlan")))
+    (do-applescript
+     (concat
+      "tell application \"Mail\"\n"
+         "set theMessage to make new outgoing message with properties {subject:\"" email-subject "\", content:\"" email-text "\", visible:true}\n"
+         "tell theMessage\n"
+            "make new to recipient with properties {name:\"Tracy Weber\", address:\"tracyw@spawnordie.com\"}\n"
+            "make new to recipient with properties {name:\"Prateek Sharma\", address:\"teek@spawnordie.com\"}\n"
+            "make new to recipient with properties {name:\"Chad Meek\", address:\"chad.meek@rht.com\"}\n"
+         "end tell\n"
+      "end tell\n"))))
+
+(defun send-timesheet (&optional p)
+  (interactive "P")
+  "Sends the current table as a timesheet to Steelhead"
+  (let ((org-enable-table-editor t))
+    (if (not (org-at-table-p))
+        (error "Must be used in a table")
+      (save-excursion
+        (let* ((beg (org-table-begin))
+               (end (org-table-end))
+               (timesheet (buffer-substring beg end)))
+          (send-rhi-email timesheet "Hours for the week of XXXX"))))))
+  
+
 (provide 'my-org-mode)
