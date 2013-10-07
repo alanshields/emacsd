1c23c2176fc6718854196ec48cf9bea290ba723a
Alan Shields <alan@alan.shields.name>
2013-10-07 13:54:12 -0700

    Remove dead code from init.el

--- init.el
+++ init.el
@@ -15,37 +15,6 @@
 
 (add-to-list 'load-path "~/.emacs.d")
 
-;; SLIME
-;; (let ((slime-dir "/opt/local/share/emacs/site-lisp/slime"))
-;;   (when (file-exists-p slime-dir)
-;;     (setq load-path (cons slime-dir load-path))
-;;     (require 'slime-autoloads)
-;;     (eval '(setq slime-lisp-implementations
-;;                  `((sbcl ("/opt/local/bin/sbcl"))
-;;                    (clisp ("/opt/local/bin/clisp")))))
-;;     (add-hook 'lisp-mode-hook
-;; 	      (lambda ()
-;; 		(cond ((not (featurep 'slime))
-;; 		       (require 'slime) 
-;; 		       (normal-mode)))))))
-;; (eval-after-load "slime"
-;;    '(slime-setup '(slime-fancy slime-banner)))
-
-;; Package-install
-;; (require 'package)
-;; (add-to-list 'package-archives
-;;              '("technomancy" . "http://repo.technomancy.us/emacs/") t)
-;; (package-initialize)
-
-;; SGML stuff
-(setq sgml-set-face t)
-(setq sgml-auto-activate-dtd t)
-(setq sgml-indent-data t)
-
-;; Bigloo's scheme mode
-;; (autoload 'bdb "bdb" "bdb mode" t)
-;; (autoload 'bee-mode "bee-mode" "bee mode" t)
-
 ;; (setq auto-mode-alist
 ;;       (append '(("\\.scm$" . bee-mode)
 ;;                 ("\\.sch$" . bee-mode)
@@ -63,22 +32,7 @@
 (global-set-key (kbd "C-z") 'eshell)
 
 
-;; Tag tables
-;;(setq tags-table-list
-;;      '("~/Documents/identity/dev-FOAD-1.0"))
-
-
-
 (put 'narrow-to-page 'disabled nil)
-
-;; rhtml
-(when (and (locate-library "mmm-mode")
-	   (locate-library "mmm-settings"))
-  (require "mmm-settings"))
-
-(add-to-list 'auto-mode-alist '("\\.rhtml$" . html-mode))
-
-(add-to-list 'auto-mode-alist '("templates/.*\\.html$" . html-mode))
 
 ;; eshell stuff
 (add-hook 'eshell-mode-hook
@@ -91,14 +45,6 @@
                      (beginning-of-line))))
              '(lambda () (define-key eshell-mode-map "\C-a" 'eshell-maybe-bol))))
 
-;; Tuareg caMeL mode
-;; (add-to-list 'load-path "~/.emacs.d/tuareg/")
-;; (setq auto-mode-alist (cons '("\\.ml\\w?" . tuareg-mode) auto-mode-alist))
-;; (autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
-;; (autoload 'camldebug "camldebug" "Run the Caml debugger" t)
-
-;;  '(default ((t (:stipple nil :background "#ffffff" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 101 :width normal :family "bitstream-bitstream vera sans mono"))))
-
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -107,24 +53,9 @@
  '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "apple" :family "Inconsolata"))))
  '(mmm-output-submode-face ((t (:background "FloralWhite" :foreground "forestgreen")))))
 
-;; color theme time
-; (require 'color-theme)
-; (color-theme-clarity)
-;; goddamn mmm-mode and its non-inheriting faces!
-; (set-face-background 'mmm-default-submode-face "dark slate gray")
-
 ; no menu bar. Werd.
 (when (not t)
   (menu-bar-mode nil))
-
-;; Don't mess with the clean directory unless you have to!
-(add-hook 'before-save-hook
-          (lambda ()
-            (when (string-match "clean_sweetapp" (buffer-file-name))
-              (when (not (yes-or-no-p
-                          (format "%s is in the clean directory.  Save anyway? "
-                                  (buffer-file-name))))
-                (error "Save not confirmed")))))
 
 (require 'uniquify)
 
@@ -154,35 +85,6 @@
  '(uniquify-buffer-name-style (quote post-forward) nil (uniquify)))
 
 
-;; Gnus!
-;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/gnus/lisp/"))
-;; (require 'gnus-load)
-;; (require 'info)
-;; (add-to-list 'Info-default-directory-list (expand-file-name "~/.emacs.d/gnus/texi"))
-
-;; bbdb
-;; (add-to-list 'load-path "~/.emacs.d/bbdb-2.35/lisp/")
-;; (require 'bbdb)
-;; (bbdb-initialize 'gnus)
-;; (add-hook 'gnus-startup-hook 'bbdb-insinuate-gnus)
-
-;; JSlint
-;; (require 'javascript-mode)
-;; (setq auto-mode-alist (cons '("\\.js\\'" . javascript-mode)
-;;              auto-mode-alist))
-(add-to-list 'load-path "~/.emacs.d/js2-mode/build/")
-(autoload 'js2-mode "js2" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
-(add-hook 'js2-mode-hook
-  (lambda ()
-    ;;; make emacs recognize the error format produced by jslint
-    (set (make-local-variable 'compilation-error-regexp-alist)
-       '(("^\\([a-zA-Z.0-9_/-]+\\):\\([0-9]+\\):\\([0-9]+\\)" 1 2 3)))
-    (set (make-local-variable 'compile-command)
-       (let ((file (file-name-nondirectory buffer-file-name)))
-          (concat "/Users/sshields/bin/jslint " file)))))
-
 ;; Org-Mode
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (setq org-export-with-LaTeX-fragments t)
@@ -196,15 +98,6 @@
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
-
-;; Puppet mode
-(load "puppet/puppet-mode-init.el")
-
-;; PHP mode
-(load "nxhtml/autostart.el")
-; nXhtml has branched off significantly from this by now.
-;(load "php-mode-improved.el")
-(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 
 (put 'narrow-to-region 'disabled nil)
 
@@ -371,18 +264,5 @@
 (when (file-exists-p "~/steelhead")
   (load "steelhead.el"))
 (put 'downcase-region 'disabled nil)
-
-;; CoffeeScript
-(add-to-list 'load-path "~/.emacs.d/coffee-mode")
-(require 'coffee-mode)
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
-
-(defun coffee-custom ()
-  "coffee-mode-hook"
- (set (make-local-variable 'tab-width) 2))
-
-(add-hook 'coffee-mode-hook
-  '(lambda() (coffee-custom)))
 
 (setq-default ispell-program-name "/usr/local/bin/aspell")
